b132a6829e34d1943fe4a49a044eaec1e5844b2f
Alex Krantz <alex@krantz.dev>
2021-02-01 13:55:44 -0800

    Add public url

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL'),
   admin: {
     auth: {
-      secret: env('JWT_SECRET', ''),
+      secret: env('JWT_SECRET'),
     },
     admin: {
       url: "/",
