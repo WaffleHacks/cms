67609f76875ccfc9ccb689504702c5bcd2a2aba1
Alex Krantz <alex@krantz.dev>
2021-02-06 17:18:54 -0800

    Serve dashboard at /dashboard

--- config/server.js
+++ config/server.js
@@ -1,14 +1,13 @@
-const removeTrailingSlash = url => (url[url.length - 1] === '/') ? url.substring(0, url.length - 1) : url;
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: removeTrailingSlash(env('PUBLIC_URL')),
+  url: env('PUBLIC_URL'),
   admin: {
     auth: {
       secret: env('JWT_SECRET'),
     },
     serveAdminPanel: true,
-    url: removeTrailingSlash(env('PUBLIC_URL'))
+    url: "/dashboard",
+    autoOpen: false
   },
 });
