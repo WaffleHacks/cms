6b0884e226cdb7e6e714769d860fe0a31976d625
Alex Krantz <alex@krantz.dev>
2021-02-10 00:42:52 -0800

    Enable sending emails through mailgun

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,15 @@
 module.exports = ({ env }) => ({
+  email: {
+    provider: 'mailgun',
+    providerOptions: {
+      apiKey: env('MAILGUN_API_KEY'),
+      domain: env('MAILGUN_DOMAIN'),
+      host: 'api.us.mailgun.net'
+    },
+    settings: {
+      defaultFrom: `cms@${env('MAILGUN_DOMAIN')}`,
+      defaultReplyTo: `no-reply@${env('MAILGUN_DOMAIN')}`
+  },
   upload: {
     provider: 'aws-s3',
     providerOptions: {
