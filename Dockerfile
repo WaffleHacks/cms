2902ef93ecdcb98f8eea925f577b93d6a9bb787d
Alex Krantz <alex@krantz.dev>
2021-05-21 00:15:21 -0700

    Containerize via docker

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM node:14-alpine
+
+WORKDIR /cms
+
+# Install dependencies
+COPY package.json yarn.lock ./
+RUN yarn install --frozen-lockfile
+
+# Apply library patches
+COPY ./patches/sso.patch ./node_modules/strapi/lib/utils/ee.js
+COPY ./patches/webhook.patch ./node_modules/strapi/lib/services/webhook-runner.js
+
+# Copy all the project files
+COPY ./api ./api
+COPY ./components ./components
+COPY ./config ./config
+COPY ./extensions ./extensions
+COPY ./public ./public
+COPY ./favicon.ico ./
+
+# Build the app
+ENV NODE_ENV production
+RUN yarn build
+
+# Prune unused dependencies/files
+RUN npm prune --production
+
+# Run the server
+EXPOSE 1337
+CMD ["yarn", "start"]
--- config/database.js
+++ config/database.js
@@ -1,24 +1,26 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const parser = require('pg-connection-string');
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: false
-      },
-      options: {
-        useNullAsDefault: true,
-        ssl: false
+module.exports = ({ env }) => {
+  const config = parser.parse(env("DATABASE_URL", "postgres://postgres:postgres@127.0.0.1:5432/postgres"))
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: false
+        },
+        options: {
+          useNullAsDefault: true,
+          ssl: false
+        },
       },
     },
-  },
-});
+  };
+}
