4527ca22567aea619c310d94b4109d910d2f822f
Alex Krantz <alex@krantz.dev>
2021-06-29 22:55:35 -0700

    Add uploading profile pictures

--- discord-oauth/hook.js
+++ discord-oauth/hook.js
@@ -4,6 +4,9 @@ const axios = require("axios");
 // From: https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
 const MANAGE_SERVER_FlAG = 0x20;
 
+// This is a hack to get around permissions to upload the user's profile picture
+const HOOK_ACCOUNTABILITY = { admin: true, ip: "127.0.0.1", userAgent: "directus-hook/discord-oauth" };
+
 /**
  * Find a user by their ID
  * @param {Object} service - the mappings service
@@ -20,11 +23,17 @@ async function findUser(service, id) {
  * Create a user and map them to their OIDC ID
  * @param {Object} users - the users service
  * @param {Object} mappings - the mapping service
+ * @param {Object} files - the files service
  * @param {string} id - the user's OIDC ID
+ * @param {string} avatar - the user's profile picture ID
  * @param {Object} userData - arbitrary user data to set on the user
  * @returns {Promise<void>}
  */
-async function createUser(users, mappings, id, userData) {
+async function createUser(users, mappings, files, id, avatar, userData) {
+  // Create the profile picture
+  userData.avatar_id = await files.importOne(`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`);
+
+  // Create the user
   const user_id = await users.createOne(userData);
   await mappings.createOne({ id, user_id });
 }
@@ -56,27 +65,20 @@ module.exports = function registerHook({ env, services }) {
       if (!authorized) return input;
 
       // Open the services
-      const files = new FilesService({ schema });
+      const files = new FilesService({ schema, accountability: HOOK_ACCOUNTABILITY });
       const mappings = new ItemsService("directus_oauth", { schema });
       const users = new UsersService({ schema });
-
-      // TODO: fix profile uploading
-      /*
-      // Upload the user's profile picture
-      const avatarId = await files.importOne(`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`);
-      */
 
       // Build the user's data
       const userData = {
         first_name: username,
         last_name: discriminator,
         email,
-        //avatar: avatarId,
       }
 
       // Create or update the user
       const userId = await findUser(mappings, id);
-      if (userId === undefined) await createUser(users, mappings, id, userData);
+      if (userId === undefined) await createUser(users, mappings, files, id, avatar, userData);
       else await users.updateOne(userId, userData);
 
       return input;
